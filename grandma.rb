42256d142bae830da0c8a07037c9d52ffad86603
Scott Ungchusri <animoogle504@gmail.com>
2018-06-25 00:49:48 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(words)
+  #did you say the magic words?
+  if(words == "I LOVE YOU GRANDMA!") #regardless of shouting
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif(words == words.upcase)
+  #shouted
+    return "NO, NOT SINCE 1938!"
+  else
+    #did not shout
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
